06595cefcaf35310bb22376a9ff9524787024e1b
kub8k <jakub.sebesta.022@pslib.cz>
2024-04-04 21:56:35 +0200

    contact form first try

--- hamburger.js
+++ hamburger.js
@@ -1,7 +1,8 @@
 function hamburger_menu() {
     const menu = document.querySelector('.header__navbar__items');
 
-    if (menu.classList.contains('hamburger--show')) {
+    if (menu.classList.contains('hamburger--show')
+    ) {
         menu.classList.remove("hamburger--show");
     } else if(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 500){
         menu.classList.add("hamburger--show");
